8b3023a26f15f9a82f86bb6d54d9f371dc9fdc0f
Pelle <pelledrijver@gmail.com>
2021-04-04 12:47:52 +0200

    Corrected tiny mistake in setup.py

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ setup(
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
-        'License :: OSI Approved :: Apache Software License'
+        'License :: OSI Approved :: Apache Software License',
         'Operating System :: OS Independent'
     ]
 )
