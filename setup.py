4017490296c4b3d2e9d5a5dca7b001aa741fc093
Pelle Drijver <pelledrijver@uber.com>
2021-05-09 17:34:27 +0200

    Improved readability and removed login functions seperate from constructor

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
@@ -11,15 +11,17 @@ setup(
     version='1.1.0',
     long_description=long_description,
     long_description_content_type="text/markdown",
-    description = "An OS-independent and easy-to-use module for creating highlight videos from trending Twitch clips. Twitch highlight videos can be created by either specifying a category or a list of streamer names.",
-    keywords = "twitch, twitch highlights, twitch clips, twitch compilation",
-    install_requires = [
-        'requests', 
+    description="An OS-independent and easy-to-use module for creating highlight videos from trending Twitch clips. "
+                "Twitch highlight videos can be created by either specifying a category or a list of streamer "
+                "names.",
+    keywords="twitch, twitch highlights, twitch clips, twitch compilation",
+    install_requires=[
+        'requests',
         'datetime',
         'moviepy>=1.0.3',
         'python-slugify>=4.0'
     ],
-    package_dir={'':'src'},
+    package_dir={'': 'src'},
     packages=['twitch_highlights'],
     license='Apache License 2.0',
     classifiers=[
